8fb2b64112f9013ded12754ef557c0003d7a38c6
AliceZ413 <491736271@qq.com>
2024-09-22 22:58:35 +0800

    vike-vue

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { resolve } from 'node:path';
 import { defineConfig } from 'vite';
+import vike from 'vike/plugin';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
   root: resolve(import.meta.dirname, './client'),
-  plugins: [vue()],
+  plugins: [vike(), vue()],
 });
